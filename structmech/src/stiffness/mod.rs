a39be6e99a8788c5281a181326274f5cfcd9faf4
Chenjox <chenjox.diepelt@gmx.de>
2022-09-26 22:56:37 +0200

    hinzufügen einer systembeschreibung

--- structmech/src/stiffness/mod.rs
+++ structmech/src/stiffness/mod.rs
@@ -1,14 +1,66 @@
+use nalgebra::DMatrix;
 /// A struct, resembling a support.
 /// The alpha is the alpha to the global coordinate system
 /// three values resemble whether its DOF is free and whether it is associated with a feather
 /// [true, true, false]
 /// [x1  , x2  , phi3 ]
-pub struct support {
+pub struct Support {
     alpha: f64,
-    is_free: [bool; 3]
+    is_free: [bool; 3],
     federnach: [f64; 3],
 }
 
-impl support {
-    
+impl Support {
+    pub fn new(alpha: f64, is_free: [bool; 3], federnach: [f64; 3]) -> Self {
+        Support {
+            alpha,
+            is_free,
+            federnach,
+        }
+    }
+    pub fn get_alpha(&self) -> f64 {
+        self.alpha
+    }
 }
+
+pub struct Point {
+    x: f64,
+    y: f64,
+}
+
+pub struct Crosssection {
+    emodul: f64,
+    area: f64,
+    ftm: f64,
+}
+
+pub struct Beam {
+    crosssection: Crosssection,
+    start_dof: [bool; 3],
+    end_dof: [bool; 3],
+}
+
+pub struct System {
+    points: Vec<Point>,
+    beam_points: Vec<[usize; 2]>,
+    beams: Vec<Beam>,
+    support_points: Vec<usize>,
+    supports: Vec<Support>,
+}
+
+pub struct SystemLoading {
+    loading_points: Vec<usize>,
+    staticloads: Vec<StaticLoad>,
+    loaded_beams: Vec<usize>,
+    lineloads: Vec<StaticLinearLineload>,
+}
+
+pub struct StaticLoad {
+    loading: [f64; 3], // x1 x2 phi3
+}
+
+pub struct StaticLinearLineload {
+    loading: [f64; 4], // Start, Ende
+}
+
+pub fn stiffnessmatrix(sys: System) {}
