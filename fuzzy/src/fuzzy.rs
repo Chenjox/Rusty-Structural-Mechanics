b964c571a1b9749e7d5a99d5e4921a0ca4c2acc6
Chenjox <chenjox.diepelt@gmx.de>
2022-10-30 01:36:02 +0200

    hinzufügen einer optimierung

--- fuzzy/src/fuzzy.rs
+++ fuzzy/src/fuzzy.rs
@@ -1,3 +1,5 @@
+const SMALL_PHI: f64 = 0.618033988749894848204586834365638118_f64;
+
 pub struct FuzzyTriangular {
     lower_value: f64,
     middle_value: f64,
@@ -75,4 +77,62 @@ pub trait FuzzyVariable {
     fn membership_of(&self, value: f64) -> f64;
 
     fn alpha_level_support(&self, alpha_level: f64) -> (f64, f64);
+}
+
+pub fn alpha_level_optimize<F>(fuzz: impl FuzzyVariable, alpha_level: f64, func: F) -> (f64, f64)
+where
+    F: Fn(f64) -> f64,
+{
+    // Verfahren des Goldenen Schnitts.
+    let suchraum = fuzz.alpha_level_support(alpha_level);
+    // Minimum
+    let max_iter = 10000;
+    let mut iter = 0;
+    // Startvariablen
+    let mut lower_guess = suchraum.0;
+    let mut upper_guess = suchraum.1;
+    let mut x1 = lower_guess + (1.0 - SMALL_PHI) * (upper_guess - lower_guess);
+    let mut x2 = lower_guess + SMALL_PHI * (upper_guess - lower_guess);
+    let mut y1 = func(x1);
+    let mut y2 = func(x2);
+    // Minimum
+    while upper_guess - lower_guess > 0.0001 && iter < max_iter {
+        if y1 < y2 {
+            upper_guess = x2;
+            x2 = x1 + (1.0 - SMALL_PHI) * (upper_guess - x1);
+            y2 = func(x2);
+        } else if y2 > y1 {
+            lower_guess = x1;
+            x1 = lower_guess + SMALL_PHI * (x2 - lower_guess);
+            y1 = func(x1);
+        }
+        iter += 1;
+    }
+
+    let min = lower_guess;
+    // Maximum
+    let mut lower_guess = suchraum.0;
+    let mut upper_guess = suchraum.1;
+    let mut x1 = lower_guess + (1.0 - SMALL_PHI) * (upper_guess - lower_guess);
+    let mut x2 = lower_guess + SMALL_PHI * (upper_guess - lower_guess);
+    let mut y1 = func(x1);
+    let mut y2 = func(x2);
+    let mut iter = 0;
+
+    while upper_guess - lower_guess > 0.0001 && iter < max_iter {
+        if y1 > y2 {
+            upper_guess = x2;
+            x2 = x1 + (1.0 - SMALL_PHI) * (upper_guess - x1);
+            y2 = func(x2);
+        } else if y2 < y1 {
+            lower_guess = x1;
+            x1 = lower_guess + SMALL_PHI * (x2 - lower_guess);
+            y1 = func(x1);
+        }
+        iter += 1;
+    }
+
+    let max = upper_guess;
+
+    (min, max)
 }
