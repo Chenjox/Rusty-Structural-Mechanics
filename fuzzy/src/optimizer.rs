c795afcbc19ccf30701301485bd56a061104d1b9
Chenjox <chenjox.diepelt@gmx.de>
2022-11-24 16:03:41 +0100

    hinzufügen von MCS versuchen

--- fuzzy/src/optimizer.rs
+++ fuzzy/src/optimizer.rs
@@ -29,8 +29,40 @@ where
     }
 } // ternary heap for better caching...
 
+// 3 oder mehr startpunkte
+
 pub fn optimize_multivariat<F>(lower_bounds: Vec<f64>, upper_bounds: Vec<f64>, func: &F)
-where F: Fn(Vec<f64>) -> f64
+where
+    F: Fn(Vec<f64>) -> f64,
 {
-    
+    // Zuerst müssen die grenzen geprüft werden:
+    let n = lower_bounds.len();
+    for i in 0..n {
+        if lower_bounds[i] >= upper_bounds[i] {
+            panic!("Upper Bounds are not strictly larger than lower bounds in all coordinates!")
+        }
+    }
+    //
+    let mut ncall = 0;
+    let mut ncloc = 0;
+    //
+    // erstellen einer Liste
+    let mut l = vec![1, n]; // Liste der Dimensionen
+    let mut L = vec![2, n]; // Liste der endpunkte oder der anzahl der partionen entlang der i-ten dimension.
+
+    // Initialisierung start!
+    // Initialisierungsliste
+    let mut x0 = Vec::new();
+
+    x0.push(lower_bounds.clone());
+    x0.push(
+        (lower_bounds)
+            .iter()
+            .zip(upper_bounds.iter())
+            .map(|t| (t.0 + t.1) / 2.0)
+            .collect(),
+    );
+    x0.push(upper_bounds.clone());
+
+    //
 }
