5d54aa481effd76c61efb680def18da6b4645a86
Jochen Diepelt <jochen.diepelt@mailbox.tu-dresden.de>
2023-01-12 08:42:11 +0100

    more stochastic analysis

--- fuzzy/src/stochastic.rs
+++ fuzzy/src/stochastic.rs
@@ -4,15 +4,6 @@ use rand::{Rng, RngCore};
 use rand_distr::Normal;
 use rand_distr::Distribution;
 
-
-fn inverse_erf(z: f64) -> f64 {
-    return PI.sqrt() * 0.5 * ( z 
-        + PI * 1.0/12.0 * z.powi(3) 
-        + 7.0 / 480.0 * PI * PI * z.powi(5)
-        + 127.0 / 40320.0 * PI * PI * PI * z.powi(7)
-        + 4369.0 / 5806080.0 * PI * PI * PI * PI * z.powi(9) );
-}
-
 pub trait StochasticVariable<R: Rng> {
     fn get_random_sample(&self, randomness: &mut R) -> f64;
     //fn get_sample_deterministic(&self) -> f64;
@@ -36,7 +27,41 @@ impl<R: Rng> StochasticVariable<R> for NormalDistributedVariable {
     }
 }
 
+pub struct ExperimentalCumulativeDistributionFunction {
+    samples: Vec<f64>
+}
+
+type ECDF = ExperimentalCumulativeDistributionFunction;
+
+impl ECDF {
+    pub fn new(samples: Vec<f64>) -> Self {
+        return ECDF {
+            samples
+        }
+    }
+
+    pub fn get_samples(&self) -> &Vec<f64> {
+        return &self.samples;
+    }
+}
+
 pub trait StochasticAnalysis<R: Rng> {
-    fn get_distributions(&self) -> Vec<Box<dyn StochasticVariable<R>>>;
+    fn get_distributions(&self) -> &Vec<Box<dyn StochasticVariable<R>>>;
     fn output_function(&self, input_vec: &Vec<f64>) -> f64;
+    //
+    fn stochastics_analysis(&self, level_reliability: f64, confidence_interval: f64, threshold_probability: f64, randomness: &mut R) -> ECDF {
+        let n_sim = (1.0 / (1.0 - level_reliability) * threshold_probability * (1.0 -threshold_probability) / (confidence_interval.powi(2))) as usize;
+        let mut res_vec = Vec::with_capacity(n_sim);
+        for _ in 0..n_sim {
+            let samples: Vec<f64> = self.get_distributions().iter()
+            .map(|f| f.get_random_sample(randomness))
+            .collect();
+            let erg = self.output_function(&samples);
+            res_vec.push(erg);
+        }
+
+        res_vec.sort_by(|a,b| a.partial_cmp(b).unwrap());
+
+        return ECDF::new(res_vec);
+    }
 }
